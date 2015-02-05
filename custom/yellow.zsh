7f9739aa6b4b961c6e7edf3f6fed4e944b2da1f7
Kirill Stytsenko <kstytsenko@yelsterdigital.com>
2015-02-05 15:18:13 +0100

    .

--- custom/yellow.zsh
+++ custom/yellow.zsh
@@ -1,6 +1,11 @@
 # kspro is the host on my work laptop
 
 if [ "$HOST" = "kspro.local" ]; then
-    alias vm="tab-color 0 150 0; cd ~/vagrant/cms && vagrant up && vagrant ssh"
-    alias vmoerp="tab-color 200 0 100; cd ~/vagrant/openerp && vagrant up && vagrant ssh"
+    alias y="mux start yelster"
+    alias vm="tab-color 0 150 0; cd ~/vagrant/cms && vagrant up > /dev/null && vagrant ssh"
+    alias vmoerp="tab-color 200 0 100; cd ~/vagrant/openerp && vagrant up > /dev/null && vagrant ssh"
+fi
+
+if [ "$HOST" = "debian" ]; then
+    alias y="deactivate ; mux start yelster"
 fi
