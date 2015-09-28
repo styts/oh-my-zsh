64590271c34b6c8c3b314acfe5a9350479575d3f
Kirill Stytsenko <kstytsenko@yelsterdigital.com>
2015-09-28 10:28:20 +0200

    .

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,6 +1,8 @@
+alias -g A='~/.oh-my-zsh/custom/aliases.zsh'
 alias b='pbcopy'
 alias c=clear
 alias cdp='cdproject'
+alias d='dirs -v | head -10 | tac'
 alias df='df -h'
 alias ds='ds -sh'
 alias du='du -h'
@@ -14,9 +16,8 @@ alias ga='git add'
 alias gaa='git add -A'
 alias gcv='git commit -v'
 alias gba='git branch -a'
-alias h='history'
-alias hpg='history | egrep'
 alias hpt='history | tail'
+alias -g G=' | grep -i'
 alias ip='ipython'
 alias ipn="ipython notebook --pprint --pylab inline"
 alias ks=ls
@@ -37,13 +38,9 @@ alias sl=ls
 alias sz='source ~/.zshrc && cd $PWD'
 alias t=`which todo.sh` -d ~/.todo.cfg
 alias v='vim'
-alias va='vim ~/.oh-my-zsh/custom/aliases.zsh'
-alias vz='vim ~/.zshrc'
 alias wo='workon'
 alias zs='sz'
-
-# GLOBAL aliases
-alias -g G='| egrep'
+alias -g Z='~/.zshrc'
 
 # git stash list, navigate with `Q`
 gstl () {
@@ -56,4 +53,31 @@ cdpy () {
   cd "$(python -c "import os.path as _, ${1}; \
           print _.dirname(_.realpath(${1}.__file__[:-1]))"
         )"
+}
+
+# history and hpg united
+h () {
+    if [[ $# -eq 0 ]] ; then
+        history
+    else
+        history | egrep "$@"
+    fi
+}
+
+# undo GIT merge
+undo_merge () {
+    git reset --merge ORIG_HEAD
+}
+alias git_undo_merge=undo_merge
+
+# change profile dynamically
+dark () {
+    echo -e "\033]50;SetProfile=Dark\a"
+    export DARK=1
+}
+
+# change profile dynamically
+light () {
+    echo -e "\033]50;SetProfile=Default\a"
+    unset DARK
 }
--- custom/yellow.zsh
+++ custom/yellow.zsh
@@ -3,9 +3,23 @@
 if [ "$HOST" = "kspro.local" ]; then
     alias y="mux start yelster"
     alias vm="tab-color 0 150 0; cd ~/vagrant/cms && vagrant up > /dev/null && vagrant ssh"
+    alias vmd="tab-color 200 200 255; cd ~/vagrant/docker && vagrant up > /dev/null && vagrant ssh"
     alias vmoerp="tab-color 200 0 100; cd ~/vagrant/openerp && vagrant up > /dev/null && vagrant ssh"
 fi
 
 if [ "$HOST" = "debian" ]; then
     alias y="mux start yelster"
 fi
+
+# branch off from a shared point, making merging with the Spanish team easier
+function qdq_branch () {
+    git checkout -b $@ $(git merge-base origin/qdq origin/master)
+}
+
+# test that the current branch merges safely into origin/qdq
+function qdq_test_branch () {
+    git checkout -b tmp origin/qdq
+    git merge @{-1} -m "test merge"
+    git checkout @{-1}
+    git branch -D tmp
+}
