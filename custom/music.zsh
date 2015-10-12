203310bb304039a71515ad648e6f6d0b91aaaf49
Kirill Stytsenko <kstytsenko@yelsterdigital.com>
2015-10-12 11:53:26 +0200

    Add `play` and `mp3` shell functions

---
+++ custom/music.zsh
@@ -0,0 +1,28 @@
+# These two functions are borrowed from here:
+# https://github.com/exogen/dotfiles/blob/730fb8e2b72b2fc3aa3d90b889874bb5195e1d07/.profile#L65
+
+
+function play {
+    # Skip DASH manifest for speed purposes. This might actually disable
+    # being able to specify things like 'bestaudio' as the requested format,
+    # but try anyway.
+    # Get the best audio that isn't WebM, because afplay doesn't support it.
+    # Use "$*" so that quoting the requested song isn't necessary.
+    youtube-dl --default-search=ytsearch: \
+               --youtube-skip-dash-manifest \
+               --output="${TMPDIR:-/tmp/}%(title)s-%(id)s.%(ext)s" \
+               --restrict-filenames \
+               --format="bestaudio[ext!=webm]" \
+               --exec=open "$*"
+}
+
+function mp3 {
+    # Get the best audio, convert it to MP3, and save it to the current
+    # directory.
+    youtube-dl --default-search=ytsearch: \
+               --restrict-filenames \
+               --format=bestaudio \
+               --extract-audio \
+               --audio-format=mp3 \
+               --audio-quality=1 "$*"
+}
