db80aca51b22a2ac9634f3acb96cdb8df0566606
Kirill Stytsenko <kirill@styts.com>
2016-03-26 20:16:52 +0100

    Replace vim with nvim on home laptop

--- custom/pro.zsh
+++ custom/pro.zsh
@@ -3,4 +3,6 @@
 if [ "$HOST" = "pro.local" ]; then
     alias vm="tab-color 0 200 0; cd; cd vagrant; vagrant up && vagrant ssh"
     alias htop="sudo htop"
+    alias vim="nvim"
+    alias v="nvim"
 fi
