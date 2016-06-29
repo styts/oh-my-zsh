457eabf268e4a83ee63ffb7e326d869ac3572ea1
Kirill Stytsenko <kirill@styts.com>
2016-06-29 18:25:52 +0200

    .

--- custom/pro.zsh
+++ custom/pro.zsh
@@ -1,6 +1,6 @@
 # pro is my 2013 rMBP
 
-if [ "$HOST" = "pro.local" ]; then
+if [[ $HOST =~ "^pro." ]]; then
     alias vm="tab-color 0 200 0; cd; cd vagrant; vagrant up && vagrant ssh"
     alias htop="sudo htop"
     alias vim="nvim"
